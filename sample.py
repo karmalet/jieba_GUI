ecbd948c14e28f32b7e1452349cb7fc18b7bc56a
karmalet <karmalet@naver.com>
2022-07-09 01:26:21 +0900

    to tackle build error
    cc.
    https://github.com/fxsjy/jieba/issues/35

---
+++ sample.py
@@ -0,0 +1,12 @@
+import jieba
+
+jieba.set_dictionary("dict.txt")
+jieba.initialize()
+
+import jieba.posseg as pseg
+
+seg_list = jieba.cut("我来到北京清华大学", cut_all=False)
+print("Default Mode: " + "/ ".join(seg_list))  # 精确模式
+
+words = pseg.cut("我爱北京天安门") #jieba默认模式
+print(list(words))
